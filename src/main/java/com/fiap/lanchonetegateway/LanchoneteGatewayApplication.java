6770c23f2a5358a0bf80351409f3f184eff6218d
Thiago <thiagobsn@gmail.com>
2024-05-10 01:59:34 -0300

    first commit

---
+++ src/main/java/com/fiap/lanchonetegateway/LanchoneteGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.lanchonetegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteGatewayApplication.class, args);
+	}
+
+}
